cf6c46adb376673e3f5b281bd3cb3ad868c4454c
Hiroshi Takeda <takejaponpon+github@gmail.com>
2013-06-03 16:03:24 +0900

    add a directory for native dlls.
    to store opencv dlls.

--- SignatureCountingTool/Program.cs
+++ SignatureCountingTool/Program.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace Signature.CountingTool
 {
     static class Program
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string lpPathName);
+
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
         static void Main()
         {
+			string directory = Path.Combine(Directory.GetCurrentDirectory(), @"Dll");
+            bool set = SetDllDirectory(directory);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ImageListingForm());
